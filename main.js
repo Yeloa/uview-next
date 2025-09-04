66249b93c2c846f7a61f2992b6f93a8b75c93428
wakge <12740482+wakge@user.noreply.gitee.com>
2025-09-04 11:28:02 +0800

    补漏

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 import App from './App';
-import zhCN from './locale/zh-Hans.json';
+import zhHans from './locale/zh-Hans.json';
+import zhHK from './locale/zh-HK.json';
 import enUS from './locale/en-US.json';
+import arSA from './locale/ar-SA.json';
+import deDE from './locale/de-DE.json';
+import esES from './locale/es-ES.json';
+import frFR from './locale/fr-FR.json';
+import jaJP from './locale/ja-JP.json';
+import koKR from './locale/ko-KR.json';
 import uView, { VueI18n, createI18n } from '@/uni_modules/uview-next';
 
 // #ifndef VUE3
@@ -9,11 +16,18 @@ Vue.use(VueI18n)
 // #endif
 
 const i18n = createI18n({
-	locale: 'zh-CN', // 默认显示语言
+	locale: 'zh-Hans', // 默认显示语言
 	fallbackLocale: 'en-US',
 	messages: {
-		'zh-CN': zhCN,
-		'en-US': enUS
+		'zh-Hans': zhHans,
+		'zh-HK': zhHK,
+		'en-US': enUS,
+		'ar-SA': arSA,
+		'de-DE': deDE,
+		'es-ES': esES,
+		'fr-FR': frFR,
+		'ja-JP': jaJP,
+		'ko-KR': koKR
 	}
 })
 
